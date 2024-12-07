5dd42213c0285f249b1e3432b0a7abc71d0929ed
arroyo235 <miarroyo23@gmail.com>
2024-12-07 23:34:46 +0100

    day3 part2

--- day3/day3.py
+++ day3/day3.py
@@ -1,11 +1,34 @@
 import re
 
-def main(lines):
+def main(file):
 
+    global do_pattern
+    do_pattern = r"(?:^|do\(\))(.*?)(?:don't\(\)|$)"
+    global mul_pattern
+    mul_pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+
+    part1(file)
+    part2(file)
+
+def part1(file):
     line_values = []
+    lines = file.split("\n")
     for line in lines:
-        pattern = "mul\((\d{1,3}),(\d{1,3})\)"
-        line_values.append(re.findall(pattern, line))
+        line_values.append(re.findall(mul_pattern, line))
+
+    result = 0
+    for values in line_values:
+        for value in values:
+            result += multiply(value)
+            
+    print(result)
+
+def part2(file):
+    line_values = []
+    matches = re.findall(do_pattern, file, flags=re.DOTALL)
+    
+    for match in matches:
+        line_values.append(re.findall(mul_pattern, match))
         
     result = 0
     for values in line_values:
@@ -13,16 +36,12 @@ def main(lines):
             result += multiply(value)
             
     print(result)
-    
+
 def multiply(numbers: tuple):
     return int(numbers[0]) * int(numbers[1])
-
     
 if __name__ == "__main__":
-    # start = time.time()
     file = open("test.txt", "r")
     file = open("input.txt", "r")
-    lines = file.read().split("\n")
-    main(lines)
-    # end = time.time()
-    # print("Time:", end-start)
+    # lines = file.read().split("\n")
+    main(file.read())
